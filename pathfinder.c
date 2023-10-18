edff6c85868ac952a9b02d4fe020f2adb5118d2e
Cyberufo44 <139644159+Cyberufo44@users.noreply.github.com>
2023-10-18 18:54:23 +0100

    Add files via upload

---
+++ pathfinder.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ *  * get_path - My designated function to get the all values from a certain path.
+ *   * The enviromental variable PATH.
+ *    *@env: Assigned pointer to enviromental variables.
+ *     * Return: All inclusive tokenized pathways for cmds.
+ *     *Last line
+ *      */
+char **get_path(char **env)
+{
+		char *pathvalue = NULL, **pathways = NULL;
+
+			unsigned int i = 0;
+
+				pathvalue = strtok(env[i], "=");
+					while (env[i])
+							{
+										if (_strcmp(pathvalue, "PATH"))
+													{
+																	pathvalue = strtok(NULL, "\n");
+																				pathways = string_cmd(pathvalue, ":");
+																							return (pathways);
+																									}
+												i++;
+														pathvalue = strtok(env[i], "=");
+															}
+						return (NULL);
+}
